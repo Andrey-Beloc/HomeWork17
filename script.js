ce812109d8631e6703e63486203ef40fc5e7443c
Andrey-Beloc <andrey.beloc@gmail.com>
2019-07-10 14:45:00 +0300

    first

---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict'
+
+var min = 60;
+var hour = min * 60;
+var threeHours = hour * 3;
+console.log( 'Три часа равны' + ' ' + threeHours + ' ' + 'секундам');
+
+
+var num = 1;
+num += 12;
+num -= 14;
+num *= 5;
+num /= 7;
+num = ++num;
+num = --num;
+alert(num);
+
+var a=10, b=2, c=5;
+console.log(a + b + c);
+
+var aa =17, bb = 10, d = '7';
+var cc = aa - bb;
+var result = cc + d;
+console.log(result)
+
+var name = 'Андрей';
+console.log('Привет,'+ ' '+ name + '!');
